3dd056695d3521487cea39553a0c15a5a1d8fbed
nthung1021 <nthung22@clc.fitus.edu.vn>
2024-12-29 18:34:04 +0700

    Update usersRoute.js (Adding routes for the Forgot Password page)

--- components/users/usersRoute.js
+++ components/users/usersRoute.js
@@ -1,10 +1,19 @@
 var express = require('express');
-var { getRegister, postRegister, getLogin, postLogin } = require('./usersController');
+var usersController = require('./usersController');
 var router = express.Router();
 
-router.get('/register', getRegister);
-router.post('/register', postRegister);
-router.get('/login', getLogin);
-router.post('/login', postLogin);
+router.get('/register', usersController.getRegister);
+router.post('/register', usersController.postRegister);
+
+router.get('/login', usersController.getLogin);
+router.post('/login', usersController.postLogin);
+
+router.get('/forgotpassword', usersController.getForgotPassword);
+router.post('/forgotpassword', usersController.postForgotPassword);
+
+router.get('/resetpassword/:token', usersController.getResetPassword);
+router.post('/resetpassword', usersController.postResetPassword);
+
+router.get('/logout', usersController.getLogout);
 
 module.exports = router;
