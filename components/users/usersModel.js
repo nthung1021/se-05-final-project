8def1f99e83ec25001d21a39d932992afff61dca
nthung1021 <nthung22@clc.fitus.edu.vn>
2024-12-29 18:33:33 +0700

    Update usersModel.js (Adding functions for Forgot Password feature)

--- components/users/usersModel.js
+++ components/users/usersModel.js
@@ -2,25 +2,68 @@ var { PrismaClient } = require('@prisma/client');
 var prisma = new PrismaClient();
 
 var findUserByUsername = async (username) => {
-    if(!username) {
-        throw new Error ('Username is required');
-    }
-
-    return prisma.user.findUnique({where: {username}});
+    if (!username) {
+        throw new Error('Username is required');
+    } 
+    return prisma.user.findUnique({ where: { username } });
 };
 
 var findUserByEmail = async (email) => {
-    if(!email) {
-        throw new Error ('Email is required');
+    if (!email) {
+        throw new Error('Email is required');
     }
-
-    return prisma.user.findUnique({where: {email}});
+    return prisma.user.findUnique({ where: { email } });
 };
 
-var createUser = async (username, email, hasedPassword) => {
+var createUser = async (username, email, hashedPassword) => {
     return prisma.user.create({
-        data: {username, email, password: hasedPassword},
+        data: { username, email, password: hashedPassword },
     });
 };
 
-module.exports = {findUserByUsername, findUserByEmail, createUser};
+var addTokenAndExpire = async (email, resetToken, expires) => {
+    if (!email) {
+        throw new Error('Email is required');
+    }
+    return prisma.user.update({
+        where: { email },
+        data: {
+            resetPasswordToken: resetToken,
+            resetPasswordExpires: expires,
+        },
+    });
+}
+
+var findTokenAndExpire = async (token) => {
+    if (!token) {
+        throw new Error('Reset-password token is empty');
+    }
+    return prisma.user.findFirst({
+        where: {
+            resetPasswordToken: token,
+            resetPasswordExpires: {
+                gte: new Date(), // Token chưa hết hạn
+            },
+        },
+    });
+}
+
+var updatePassword = async (userid, hashedPassword) => {
+    return prisma.user.update({
+        where: { id: userid },
+        data: {
+            password: hashedPassword,
+            resetPasswordToken: null,
+            resetPasswordExpires: null,
+        },
+    });
+}
+
+module.exports = {
+    findUserByUsername, 
+    findUserByEmail, 
+    createUser, 
+    addTokenAndExpire, 
+    findTokenAndExpire, 
+    updatePassword
+};
